fec74d8de9c3a5ff22dd2f7259f3ca6787de9a50
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-04-22 20:51:30 -0700

    Added some comments

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include "sum.h"
+#include <unistd.h>
 
-int main() {
-    int a = 1;
-    int b = 1;
-    int z = sum(a,b);
-    printf("Hello, World!\n %d",z);
-    return 0;
+int main()
+{
+
+
+    while(true)
+    {
+        //user starts the applicatioin
+        //Chooses what to open or where to start it
+        //can then chooses to do some function
+        //then can do more functions or exit the program
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("Current working directory: %s\n", cwd);
+        } else {
+            perror("getcwd() error");
+            return 1;
+        }
+        return 0;
+    }
 }
